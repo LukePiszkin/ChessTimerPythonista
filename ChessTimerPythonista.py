fe6f640ce43824653296d020669e28d26af8ed95
Luke Piszkin <lpiszkin@gmail.com>
2019-05-04 20:58:56 -0700

    Functional timer with notification

--- ChessTimerPythonista.py
+++ ChessTimerPythonista.py
@@ -1,23 +1,57 @@
-import ui, time
+import ui, time, notification, sound
 
 view = ui.View()
 view.size_to_fit()
 view.background_color = 'white'
 view.flex = 'WH'
 
-player1button = ui.Button()
-player1button.title = time1
-player1button.action  = tap 
+time1 = 5
+time2 = 0
+time3 = 0
 
-def tap(sender):
-    pass
+
+player1button = ui.Button()
+player1button.border_color = '#198054#'
+player1button.border_width = 5
+player1button.title = str(time1) + ':' + str(time2) + str(time3)
+
+
 
 def timer1(sender):
-    if time1 < 0:
-        time1 -= 1
-        player1button.title = time1
-        ui.delay(timer1, 1)
+    global time1
+    global time2
+    global time3
+    if time3 == 0:
+        time3  = 9
+        if time2 == 0:
+            time2 = 5
+             player1button.title = str(time1) + ':' + str(time2) + str(time3)
+             timer1(sender)
+        if time2 != 0:
+            time2 -=1 
+             player1button.title = str(time1) + ':' + str(time2) + str(time3)
+        timer1(sender)
+        time.sleep(1)
+        player1button.title = str(time1) + ':' + str(time2) + str(time3)
+        timer1(sender)
+    if time2 == 0:
+        time1 -=1
+         player1button.title = str(time1) + ':' + str(time2) + str(time3)
+        timer1(sender)
+    if time3 > 0:
+        time3 -= 1
+        time.sleep(1)
+        player1button.title = str(time1) + ':' + str(time2) + str(time3)
+        timer1(sender)
+    if time1 == 0:
+        notification.schedule(Flag Down! Player 2 wins!)
 
+
+
+    
+        
+
+player1button.action = timer1
 
 view.add_subview(player1button)
 view.present()
